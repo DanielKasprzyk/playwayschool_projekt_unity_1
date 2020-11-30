365509d8b4543fb4c1ebd1296d7f846f34776859
Daniel Kasprzyk <dkwarmaster3@gmail.com>
2020-11-30 20:54:43 +0100

    project files

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float maxInteractionDistance = 2f;
+
+    public virtual bool CanInteract(Transform player)
+    {
+        return (player != null && Vector3.Distance(transform.position, player.transform.position) <= maxInteractionDistance);
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interakcja");
+    }
+}
---
+++ Assets/Scripts/PlayerInteractionComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionComponent : MonoBehaviour
+{
+    private Interactable interactable;
+    void Start()
+    {
+        interactable = null;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && interactable != null)
+        {
+            if (interactable.CanInteract(transform))
+            {
+                interactable.Interact();
+            }
+        }
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        interactable = collider.gameObject.GetComponent<Interactable>();
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Interactable>() != null)
+        {
+            interactable = null;
+        }
+    }
+}
